51736b52440d74b514802dfdd9f6c4975d4194f1
DivyantPatel <122143641+DivyantPatel@users.noreply.github.com>
2024-06-14 21:50:02 -0700

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,24 @@
+print( "Hello World")
+
+name = "Divyant"
+print( "Hello",name,"!")	
+print( "Hello " + name + "!" )
+
+name = 42
+print("Hello",42,"!")
+print( "Hello " + str(42) + "!")
+
+
+fave_food1 = "Pasta"
+fave_food2 = "Pizza"
+print( "I love to eat {} and {}.".format(fave_food1,fave_food2) )
+print( f"I love to eat {fave_food1} and {fave_food2}." ) 
+
+
+	
+	
+
+
+
+
+
